b6df3e2817cd9635765dd921910f1ed3b7cec997
Mohamed Ouhami <mohamed.ouhami2001@gmail.com>
2024-11-03 21:49:14 +0000

    first commit

---
+++ util/utils.py
@@ -0,0 +1,23 @@
+# In this script, we will get the data from the API
+import requests
+import json
+from dotenv import load_dotenv
+import os
+# Test fetching the data from the API
+
+# Load the .env file
+load_dotenv()
+
+def authenticate():
+    WEATHER_API_SECRET = os.environ.get("WEATHER_API_KEY")
+    print(WEATHER_API_SECRET)
+
+def getData():
+    result = requests.get("http://api.weatherstack.com/current?access_key=eab271961a6cf4253bd8ecbf8e18e627&query=Casablanca")
+    json_result = json.loads(result.text)
+    pretty_result = json.dumps(json_result['current'],indent=2)
+    print(pretty_result)
+    
+authenticate()
+getData()
+
